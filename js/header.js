e68b480877848b94e32fc9250fdd16a70b884dfe
kimtooty <csb14110@gmail.com>
2026-01-15 19:49:22 +0900

    feature section complete

---
+++ js/header.js
@@ -0,0 +1,40 @@
+/** Header Change Effect **/
+const header = document.querySelector('#header');
+
+const stickyHeader = () => {
+  const scry = window.scrollY;
+  if (scry > 0) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+};
+
+window.addEventListener('scroll', stickyHeader);
+
+/* Menu Elements Clone For Mobile */
+
+const mobileMenus = document.querySelector('.mobile-menus');
+const navs = document.querySelector('.nav-lists').cloneNode(true);
+const info = document.querySelector('.info').cloneNode(true);
+
+mobileMenus.appendChild(navs);
+mobileMenus.appendChild(info);
+
+/** Movile Menu Toggle **/
+const mobileBtn = document.querySelector('.mobile-btn');
+const menuHeight = mobileMenus.scrollHeight;
+
+toggleMobileBtn = (e) => {
+  const target = e.currentTarget;
+  target.classList.toggle('active');
+  if (target.classList.contains('active')) {
+    target.classList.remove('not-active');
+    mobileMenus.style.height = menuHeight + 'px';
+  } else {
+    target.classList.add('not-active');
+    mobileMenus.style.height = 0;
+  }
+};
+
+mobileBtn.addEventListener('click', toggleMobileBtn);
